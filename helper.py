d29325d1df4b7eddeb974c627ea9baadb6e7854e
gsaboia <gsaboia@yahoo.com>
2024-06-11 14:50:15 -0300

    feat: updated matplotlib usage in helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
+import matplotlib
 from IPython import display
+
+matplotlib.use('Agg')  # Use TkAgg backend for interactive plots
+import matplotlib.pyplot as plt
 
 plt.ion()
 
@@ -8,11 +11,14 @@ def plot(scores, mean_scores):
     display.display(plt.gcf())
     plt.clf()
     plt.title('Training...')
-    plt.xlabel('Number of games')
+    plt.xlabel('Number of Games')
     plt.ylabel('Score')
-    plt.plot(scores)
-    plt.plot(mean_scores)
+    plt.plot(scores, label='Scores')
+    plt.plot(mean_scores, label='Mean Scores')
     plt.ylim(ymin=0)
-    plt.text(len(scores) - 1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores) - 1, mean_scores[-1], str(mean_scores[-1]))
-    
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.legend()
+    plt.savefig("training.png")
+    plt.show(block=False)
+    plt.pause(.1)
